481eff801afafce8617a658fc69c40e3d4b485d8
mel-muhina <mel.muhina@gmail.com>
2024-03-20 19:58:14 +0000

    verison 1.0 // All features tested

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './vars.css';
+import './App.css';
+
 import reportWebVitals from './reportWebVitals';
+import "@fontsource/outfit"
+import "@fontsource/roboto"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/utils.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//export const getImageUrl = (path) => {
+//return new URL(`../assets/${path}`, import.meta.url).href;
+//};
+
+export const getImageUrl = (fileName) => {
+    const imageUrl = `/assets/${fileName}`;
+    console.log('Image URL:', imageUrl);
+    return imageUrl;
+}
