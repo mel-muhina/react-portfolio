511606939246559315a9bc0fb89e42ae37a0d80b
mel-muhina <mel.muhina@gmail.com>
2024-03-20 22:14:27 +0000

    Hero broken fix

---
+++ build/precache-manifest.65f7e79703d8ab052db6ab597e3146e8.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba692040044469827f993c7f3477ab4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e360677b916ab3a9a14c",
+    "url": "/react-portfolio/static/css/2.442ad880.chunk.css"
+  },
+  {
+    "revision": "1c39c17d4d95bda2a293",
+    "url": "/react-portfolio/static/css/main.5d9fa8f3.chunk.css"
+  },
+  {
+    "revision": "e360677b916ab3a9a14c",
+    "url": "/react-portfolio/static/js/2.a927779a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-portfolio/static/js/2.a927779a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b0807f23b43aebec91",
+    "url": "/react-portfolio/static/js/3.d0d4a09f.chunk.js"
+  },
+  {
+    "revision": "1c39c17d4d95bda2a293",
+    "url": "/react-portfolio/static/js/main.9bced3f3.chunk.js"
+  },
+  {
+    "revision": "dbf03e0a92e7b93a953b",
+    "url": "/react-portfolio/static/js/runtime-main.29f37f73.js"
+  },
+  {
+    "revision": "f9908342957666e63327caefe73b770f",
+    "url": "/react-portfolio/static/media/PreviousPortfolio.f9908342.JPG"
+  },
+  {
+    "revision": "f01bfef1f67ab565de4e79b433baf570",
+    "url": "/react-portfolio/static/media/ProfileMini.f01bfef1.jpg"
+  },
+  {
+    "revision": "e38e0e89e2411b5f00d6477331d76184",
+    "url": "/react-portfolio/static/media/aboutImage.e38e0e89.png"
+  },
+  {
+    "revision": "8d2cc13ed3f56b2c2305944e7cc04477",
+    "url": "/react-portfolio/static/media/beautybeginsscreen.8d2cc13e.jpg"
+  },
+  {
+    "revision": "eb5232c038a480307c44e3a8e3fd91c8",
+    "url": "/react-portfolio/static/media/bluesclues.eb5232c0.jpg"
+  },
+  {
+    "revision": "e953e394f9041ae354042179f225363d",
+    "url": "/react-portfolio/static/media/numgame.e953e394.JPG"
+  },
+  {
+    "revision": "2e30371bb470909da20d6f5ba96cdc27",
+    "url": "/react-portfolio/static/media/outfit-latin-400-normal.2e30371b.woff"
+  },
+  {
+    "revision": "bce20846c6d19adedead2feb2d4773f0",
+    "url": "/react-portfolio/static/media/outfit-latin-400-normal.bce20846.woff2"
+  },
+  {
+    "revision": "2dd8ef05a094c3de312b377d3586b786",
+    "url": "/react-portfolio/static/media/outfit-latin-ext-400-normal.2dd8ef05.woff"
+  },
+  {
+    "revision": "5104713a692c841eb225782b3f2337e8",
+    "url": "/react-portfolio/static/media/outfit-latin-ext-400-normal.5104713a.woff2"
+  },
+  {
+    "revision": "40c76b36a1db219fd7643132f0b71e0f",
+    "url": "/react-portfolio/static/media/project.40c76b36.png"
+  },
+  {
+    "revision": "13cb0759b52b47bd887574a9cc55bd95",
+    "url": "/react-portfolio/static/media/redux.13cb0759.png"
+  },
+  {
+    "revision": "c57da4a6af18c11cc7ac408c6440e679",
+    "url": "/react-portfolio/static/media/roboto-cyrillic-400-normal.c57da4a6.woff"
+  },
+  {
+    "revision": "d9ac47c7e500fb7083b8d595eaf6fe12",
+    "url": "/react-portfolio/static/media/roboto-cyrillic-400-normal.d9ac47c7.woff2"
+  },
+  {
+    "revision": "13e4326a795bf5a1e996c8f9bbe6055c",
+    "url": "/react-portfolio/static/media/roboto-cyrillic-ext-400-normal.13e4326a.woff"
+  },
+  {
+    "revision": "c00467dc3792a8ab586955a3faefcac9",
+    "url": "/react-portfolio/static/media/roboto-cyrillic-ext-400-normal.c00467dc.woff2"
+  },
+  {
+    "revision": "28668857bef1b85c5748a482cf9b74af",
+    "url": "/react-portfolio/static/media/roboto-greek-400-normal.28668857.woff2"
+  },
+  {
+    "revision": "e5bdd5a0839957cd8e75c94055313fd8",
+    "url": "/react-portfolio/static/media/roboto-greek-400-normal.e5bdd5a0.woff"
+  },
+  {
+    "revision": "35de3738b76d249ed060dd3d0f9286be",
+    "url": "/react-portfolio/static/media/roboto-greek-ext-400-normal.35de3738.woff2"
+  },
+  {
+    "revision": "3d5467e6c9c5cb52ff5940dcdb34e5ba",
+    "url": "/react-portfolio/static/media/roboto-greek-ext-400-normal.3d5467e6.woff"
+  },
+  {
+    "revision": "15d9f621c3bd1599f0169dcf0bd5e63e",
+    "url": "/react-portfolio/static/media/roboto-latin-400-normal.15d9f621.woff2"
+  },
+  {
+    "revision": "c7ab406db23cf2d880297f07a0ae35d3",
+    "url": "/react-portfolio/static/media/roboto-latin-400-normal.c7ab406d.woff"
+  },
+  {
+    "revision": "87ace20058325aa069320aa4af875dff",
+    "url": "/react-portfolio/static/media/roboto-latin-ext-400-normal.87ace200.woff2"
+  },
+  {
+    "revision": "b748a1f16098ad1d03c21b6a056b46f6",
+    "url": "/react-portfolio/static/media/roboto-latin-ext-400-normal.b748a1f1.woff"
+  },
+  {
+    "revision": "08a0a460f57b4d4bf3e90e5047be8039",
+    "url": "/react-portfolio/static/media/roboto-vietnamese-400-normal.08a0a460.woff"
+  },
+  {
+    "revision": "ca3b09b62fda648a4511700413313fd0",
+    "url": "/react-portfolio/static/media/roboto-vietnamese-400-normal.ca3b09b6.woff2"
+  },
+  {
+    "revision": "2ba8786216ef8474c5b8fc609ea28ba6",
+    "url": "/react-portfolio/static/media/style.2ba87862.JPG"
+  },
+  {
+    "revision": "7b5e383e2bf05ccfaf9715a7eb57d13c",
+    "url": "/react-portfolio/static/media/teacozy.7b5e383e.JPG"
+  },
+  {
+    "revision": "7fd6c2e90bf4d0f8f0b371a6ed2f0647",
+    "url": "/react-portfolio/static/media/web1.7fd6c2e9.jpg"
+  }
+]);
